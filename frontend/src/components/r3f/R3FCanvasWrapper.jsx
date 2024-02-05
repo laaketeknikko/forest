ac106bb3c3f17686fa6f2cf39658f4471e689e87
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2024-02-05 20:45:21 +0200

    Added basic idle detection to stop rendering if user idle.

--- frontend/src/components/r3f/R3FCanvasWrapper.jsx
+++ frontend/src/components/r3f/R3FCanvasWrapper.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import { Canvas, useFrame } from "@react-three/fiber"
 import { OrbitControls } from "@react-three/drei"
 
@@ -9,17 +11,47 @@ import { allPlayerCharactersAtom } from "../../game/state/jotai/characters"
 import { ActionHelper } from "./ActionHelpers/ActionHelper"
 import { allEnemiesAtom } from "../../game/state/jotai/enemies"
 
-//const DisableRender = () => useFrame(() => null, 1000)
+import { useIdleTimer } from "react-idle-timer"
+
+const DisableRender = () => useFrame(() => null, 1000)
 
 const R3FCanvasWrapper = () => {
    const [allPlayerCharactersValue] = useAtom(allPlayerCharactersAtom)
    const [allEnemiesValue] = useAtom(allEnemiesAtom)
+   const [pauseAnimation, setPauseAnimation] = useState(false)
+
+   const onIdle = () => {
+      setPauseAnimation(true)
+   }
+
+   const onAction = () => {
+      setPauseAnimation(false)
+   }
+
+   useIdleTimer({
+      onIdle: onIdle,
+      onAction: onAction,
+      timeout: 1000,
+      event: [
+         "keydown",
+         //'wheel',
+         //'DOMMouseScroll',
+         //'mousewheel',
+         "mousedown",
+         "touchstart",
+         "touchmove",
+         "MSPointerDown",
+         "MSPointerMove",
+         "visibilitychange",
+      ],
+   })
 
    return (
       <Canvas
          camera={{ position: [1, 4, 5] }}
          style={{ backgroundColor: "rgb(31, 27, 22)" }}
       >
+         {pauseAnimation && <DisableRender />}
          <axesHelper />
          <OrbitControls />
          <ambientLight args={["white", 1]} />
