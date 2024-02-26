8b789fc70754d671fa9c08f8b515c190c4d43161
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2024-02-26 14:07:04 +0200

    Mocked up a debriefing screen.

--- frontend/src/components/Debriefing/Debriefing.tsx
+++ frontend/src/components/Debriefing/Debriefing.tsx
@@ -1,18 +1,75 @@
 import Button from "@mui/material/Button"
 import Container from "@mui/material/Container"
 import Typography from "@mui/material/Typography"
-import { gameExecutionStateAtom } from "../../game/state/jotai/gameState"
+import {
+   activeSaveGameConfigAtom,
+   gameExecutionStateAtom,
+} from "../../game/state/jotai/gameState"
 import { useAtom } from "jotai"
 import { MainWindowDisplayStatus } from "../../config/types"
+import Grid2 from "@mui/material/Unstable_Grid2/Grid2"
+import { Divider } from "@mui/material"
+import { theme } from "../../styles/mui/theme"
 
 const Debriefing = () => {
    const [gameExecutionState, setGameExecutionState] = useAtom(
       gameExecutionStateAtom
    )
+   const [saveGame] = useAtom(activeSaveGameConfigAtom)
 
    return (
       <Container>
-         <Typography variant="h3">You won!!!!!!</Typography>
+         <Typography variant="h3" color="primary" textAlign={"center"}>
+            Debriefing
+         </Typography>
+         <Grid2 columns={24} container justifyItems={"center"}>
+            <Grid2 xs={12}>
+               <Typography variant="h5" textAlign={"center"}>
+                  {saveGame.scenario.name}
+               </Typography>
+               <Divider
+                  variant="middle"
+                  color="primary"
+                  aria-hidden="true"
+                  sx={{ borderColor: theme.palette.primary.main }}
+               />
+               <Typography variant="h6">
+                  Result:{" "}
+                  {gameExecutionState.scenario.won ? (
+                     <Typography
+                        variant="body1"
+                        component="span"
+                        background-color="primary"
+                     >
+                        Victory
+                     </Typography>
+                  ) : (
+                     <Typography
+                        variant="body1"
+                        component="span"
+                        color="primary"
+                     >
+                        Defeat
+                     </Typography>
+                  )}
+               </Typography>
+
+               <Typography variant="h5">Enemies defeated</Typography>
+            </Grid2>
+
+            <Grid2 xs={12}>
+               <Typography variant="h5" textAlign={"center"}>
+                  Participating characters
+               </Typography>
+               <Divider
+                  variant="middle"
+                  color="primary"
+                  aria-hidden="true"
+                  sx={{ borderColor: theme.palette.primary.main }}
+               />
+            </Grid2>
+         </Grid2>
+
          <Button
             onClick={() => {
                setGameExecutionState({
--- frontend/todo/todo.md
+++ frontend/todo/todo.md
@@ -1,19 +1,5 @@
 TODO: Implement a global notification / log display.
 
-TODO: Add some sort of a progression or requirements for scenarios.
-
-TODO: Make a way to win a scenario?
-You would need victory conditions, checking conditions, like when enemy dies.
-Something like:
-
--  make a hook, useScenarioWinCondition
--  in hook, useEffects reacts to changes in conditions
--  if condition is true, set flag to global game state
--  On scenario configs, define victory conditions, like:
-   -  type: "enemy"
-   -  name: "Miinii"
-   -  state: "dead"
-
 TODO: Making a save game needs to be rethought.
 At the moment the character, enemy and scenario data in the save game
 is simply overwritten. This won't do if there ever are characters
@@ -24,14 +10,14 @@ TODO: Update save data when scenario is started.
 
 TODO: implement a victory screen on scenario over.
 
-TODO: Look into using jotai selectAtom.
-
 TODO: Debug scenario progression.
 
 TODO: Implement some frontend tests.
 
-TODO: Have characters face the active character.
-
-TODO: Center helper grid on real grid.
-
 TODO: Combat log?
+
+TODO: performance optimizations.
+
+TODO: prevent characters from leaving the arena.
+
+TODO: Victory conditions don't seem to reset between scenarios.
