a89ceb32018d9a59809e54d05b821f8078266863
laaketeknikko <139527598+laaketeknikko@users.noreply.github.com>
2024-03-02 13:41:06 +0200

    Improved debriefing screen.

--- frontend/src/components/Debriefing/Debriefing.tsx
+++ frontend/src/components/Debriefing/Debriefing.tsx
@@ -45,8 +45,9 @@ const Debriefing = () => {
          <Stack
             direction={"row"}
             spacing={2}
-            justifyContent={"center"}
-            marginTop={3}
+            justifyContent={"space-around"}
+            marginTop={10}
+            marginBottom={10}
          >
             <Typography variant="h6">
                Result:{" "}
@@ -82,54 +83,66 @@ const Debriefing = () => {
                </Typography>
             </Typography>
          </Stack>
-         <Grid2
-            columns={24}
-            container
-            justifyItems={"center"}
-            sx={{ marginTop: 3 }}
-         >
-            <Grid2 xs={12}>
-               <Typography variant="h5">Enemies defeated</Typography>
-               <Divider
-                  variant="middle"
-                  color="primary"
-                  aria-hidden="true"
-                  sx={{ borderColor: theme.palette.primary.main }}
-               />
-               {enemyAtoms.map((atom) => {
-                  return (
-                     <DebriefingEntityCard
-                        key={atom.toString()}
-                        entityAtom={atom}
-                        direction="vertical"
-                     />
-                  )
-               })}
-            </Grid2>
-
-            {/** Party section*/}
-            <Grid2 xs={12}>
-               <Typography variant="h5" textAlign={"center"}>
-                  Participating characters
-               </Typography>
-               <Divider
-                  variant="middle"
-                  color="primary"
-                  aria-hidden="true"
-                  sx={{ borderColor: theme.palette.primary.main }}
-               />
-               <Stack>
-                  {characterAtoms.map((atom) => {
+         <Grid2 columns={24} container sx={{ marginTop: 3 }}>
+            {/** Enemies section
+             *
+             */}
+            <Grid2 xs={12} container columns={24}>
+               <Grid2 xs={24} height={"min-content"}>
+                  <Typography variant="h5" textAlign="center">
+                     Enemies
+                  </Typography>
+                  <Divider
+                     variant="middle"
+                     color="primary"
+                     aria-hidden="true"
+                     sx={{ borderColor: theme.palette.primary.main }}
+                  />
+               </Grid2>
+               <Grid2 container columns={24} justifyContent={"center"} xs={24}>
+                  {enemyAtoms.map((atom) => {
                      return (
-                        <Box component="div" key={atom.toString()}>
+                        <Grid2 key={atom.toString()} xs={12} sm={8} lg={6}>
                            <DebriefingEntityCard
                               entityAtom={atom}
                               direction="vertical"
                            />
-                        </Box>
+                        </Grid2>
                      )
                   })}
-               </Stack>
+               </Grid2>
+            </Grid2>
+
+            {/** Party section
+             *
+             */}
+            <Grid2 xs={12} container columns={24}>
+               <Grid2 xs={24}>
+                  <Typography variant="h5" textAlign={"center"}>
+                     Party
+                  </Typography>
+
+                  <Divider
+                     variant="middle"
+                     color="primary"
+                     aria-hidden="true"
+                     sx={{ borderColor: theme.palette.primary.main }}
+                  />
+               </Grid2>
+               <Grid2 container columns={24} alignItems="end">
+                  {characterAtoms.map((atom) => {
+                     return (
+                        <Grid2 key={atom.toString()} xs={12} sm={8} lg={6}>
+                           <Box component="div">
+                              <DebriefingEntityCard
+                                 entityAtom={atom}
+                                 direction="vertical"
+                              />
+                           </Box>
+                        </Grid2>
+                     )
+                  })}
+               </Grid2>
             </Grid2>
          </Grid2>
 
